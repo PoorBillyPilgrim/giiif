f4664842a9aea5370ef952a917599a6a156a81c4
Tyler Jones <tjjones93@gmail.com>
2021-10-02 16:04:14 -0400

    refactor this.body to be populated with setBody method

--- src/manifest/annotation.js
+++ src/manifest/annotation.js
@@ -7,21 +7,33 @@ class Annotation extends Template {
   /**
      * Create an Annotation
      * @param {Object} info
-     * @param {string} info.id - unique URL for resource type
+     * @param {String} info.id - unique URL for resource type
      */
   constructor (info) {
     super(info.id, 'Annotation')
     this.motivation = 'painting'
     this.body = {
-      id: null,
-      type: 'Image',
-      format: 'image/jpeg'
+      id: null, // should be entire Image API URL, or if not from IIIF image server, just actual location of image
+      type: null,
+      format: null,
+      height: null,
+      width: null,
+      service: []
     }
     this.target = null
   }
 
   setTarget (canvas) {
     this.target = canvas
+  }
+
+  setBody (options) {
+    this.body.id = options.id
+    this.body.type = options.type
+    this.body.format = options.format
+    this.body.height = options.height
+    this.body.width = options.width
+    this.body.service.push(options.service)
   }
 }
 
